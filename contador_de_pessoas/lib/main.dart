2db51b9e3c9c9af4620bf31490be962239943772
ssj4dofuturo <felip3.fl@gmail.com>
2020-11-02 15:16:19 -0300

    Aula 41

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -3,31 +3,52 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text(
-            "Pessoas: 0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          Image.asset(
+            "images/restaurant.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
           ),
-         Row(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: <Widget>[
-             FlatButton(
-                 onPressed: (){},
-                 child: Text(
-                   "+1", style: TextStyle(fontSize: 40.0, color: Colors.white),)),
-             FlatButton(
-                 onPressed: (){},
-                 child: Text(
-                   "-1", style: TextStyle(fontSize: 40.0, color: Colors.white),))
-           ],
-         ),
-          Text(
-            "Pode Entrar",
-            style: TextStyle(color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 30.0),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoas: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          "+1",
+                          style: TextStyle(fontSize: 40.0, color: Colors.white),
+                        )),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          "-1",
+                          style: TextStyle(fontSize: 40.0, color: Colors.white),
+                        )),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode Entrar",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
+              )
+            ],
           )
         ],
       )));
