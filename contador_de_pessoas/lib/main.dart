3def61257d2a75ebba746dc5c9712c326836245f
ssj4dofuturo <felip3.fl@gmail.com>
2020-11-02 14:48:33 -0300

    Aula 39

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    title: "Contador de Pessoas",
-    home: Container(color: Colors.white,)
-  ));
+      title: "Contador de Pessoas",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Pode Entrar",
+            style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30.0),
+          )
+        ],
+      )));
 }
