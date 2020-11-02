6f67ab56ab9ec14c1cd98d46213a7a05c22fcfbb
ssj4dofuturo <felip3.fl@gmail.com>
2020-11-02 14:59:03 -0300

    Aula 40

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -10,9 +10,24 @@ void main() {
             "Pessoas: 0",
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
+         Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             FlatButton(
+                 onPressed: (){},
+                 child: Text(
+                   "+1", style: TextStyle(fontSize: 40.0, color: Colors.white),)),
+             FlatButton(
+                 onPressed: (){},
+                 child: Text(
+                   "-1", style: TextStyle(fontSize: 40.0, color: Colors.white),))
+           ],
+         ),
           Text(
             "Pode Entrar",
-            style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30.0),
+            style: TextStyle(color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
           )
         ],
       )));
