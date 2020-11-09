5620791d276cdb15b913b7433ff19715911475cb
ssj4dofuturo <felip3.fl@gmail.com>
2020-11-09 19:53:52 -0300

    Aula 51

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -24,6 +24,7 @@ class _HomeState extends State<Home> {
     heightController.text = "";
     setState(() {
       _infoText = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();
     });
   }
 
