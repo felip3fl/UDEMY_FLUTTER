85f9c75b311c37597eb2c7162649bd6a0628c403
ssj4dofuturo <felip3.fl@gmail.com>
2020-11-09 19:52:31 -0300

    Aula 50 pt2

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -79,7 +79,6 @@ class _HomeState extends State<Home> {
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.green, fontSize: 25.0),
                 controller: weightController,
-
                 validator: (value) {
                   if(value.isEmpty){
                     return "Insira seu peso!";
