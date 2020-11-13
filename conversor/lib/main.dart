5aa159446e5e3a220b09abba10e9b9317dcceff2
ssj4dofuturo <felip3.fl@gmail.com>
2020-11-13 20:15:30 -0300

    Aula 58

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -2,14 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:async/async.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 
 const request = "https://api.hgbrasil.com/finance?format=json&Key=ceb4e8fb";
 
 void main() async{
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
-  
   runApp(MaterialApp(
-    home: Container(),
+    home: Home()
   ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+
+    );
+  }
+}
+
+
+Future<Map> getData() async{
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
