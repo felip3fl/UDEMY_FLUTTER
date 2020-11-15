8fb41d8ab1fecbfd76a83ba43b22dfd75844dd94
ssj4dofuturo <felip3.fl@gmail.com>
2020-11-15 17:09:28 -0300

    Aula 62

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -29,8 +29,24 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
   double dolar;
   double euro;
+
+  void _realChanged(String text){
+    print(text);
+  }
+
+  void _dolarChanged(String text){
+    print(text);
+  }
+
+  void _euroChanged(String text){
+    print(text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,41 +90,11 @@ class _HomeState extends State<Home> {
                   children: <Widget>[
                     Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
                     Divider(),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Reais",
-                          labelStyle: TextStyle(color: Colors.amber),
-                          border: OutlineInputBorder(),
-                        prefixText: "R\$"
-                      ),
-                      style: TextStyle(
-                        color: Colors.amber, fontSize: 25.0
-                      ),
-                    ),
+                    buildTextField("Reais","R", realController, _realChanged),
                     Divider(),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: "Dólares",
-                          labelStyle: TextStyle(color: Colors.amber),
-                          border: OutlineInputBorder(),
-                          prefixText: "US\$"
-                      ),
-                      style: TextStyle(
-                          color: Colors.amber, fontSize: 25.0
-                      ),
-                    ),
+                    buildTextField("Dólares","US", dolarController, _dolarChanged),
                     Divider(),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: "Euros",
-                          labelStyle: TextStyle(color: Colors.amber),
-                          border: OutlineInputBorder(),
-                          prefixText: "E\$"
-                      ),
-                      style: TextStyle(
-                          color: Colors.amber, fontSize: 25.0
-                      ),
-                    )
+                    buildTextField("Euros","E", euroController, _euroChanged),
                   ],
                 ),
               );
@@ -123,4 +109,21 @@ class _HomeState extends State<Home> {
 Future<Map> getData() async{
   http.Response response = await http.get(request);
   return json.decode(response.body);
+}
+
+Widget buildTextField(String label, String prefix, TextEditingController c, Function f){
+  return TextField(
+    controller: c,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix
+    ),
+    style: TextStyle(
+        color: Colors.amber, fontSize: 25.0
+    ),
+    onChanged: f,
+    keyboardType:  TextInputType.number,
+  );
 }
