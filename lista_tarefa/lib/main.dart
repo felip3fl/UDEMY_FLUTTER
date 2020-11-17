aa0f8179a4c39052b8b34f96b75f754e6a6204ee
ssj4dofuturo <felip3.fl@gmail.com>
2020-11-17 20:45:41 -0300

    Aula 69

--- lista_tarefa/lib/main.dart
+++ lista_tarefa/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main(){
   runApp(MaterialApp(
@@ -12,8 +15,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data)
+  }
+
+  Future<String> _readData() async {
+    try{
+      final file = await _getFile();
+      return file.readAsString();
+    }catch(e){
+      return null;
+    }
+
+  }
+
 }
+
