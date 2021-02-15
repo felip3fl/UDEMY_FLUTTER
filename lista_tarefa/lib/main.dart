1d62033e447cd2a393d69cff877720fa341f276e
ssj4dofuturo <felip3.fl@gmail.com>
2021-02-15 13:51:44 -0300

    Aula 71

--- lista_tarefa/lib/main.dart
+++ lista_tarefa/lib/main.dart
@@ -15,7 +15,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = [];
+  List _toDoList = ["Daniel","Marcos","Felipe","João","Paulo"];
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +46,21 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
+          ),
+          Expanded(
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: (context, index){
+                    return CheckboxListTile(
+                      title: Text(_toDoList[index]["title"]),
+                      value: _toDoList[index]["ok"],
+                      secondary: CircleAvatar(
+                        child: Icon(_toDoList[index]["ok"] ?
+                        Icons.check : Icons.error
+                        ),),
+                    );
+                  }),
           )
         ],
       ),
