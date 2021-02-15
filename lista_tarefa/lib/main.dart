990625ea14e2c95d898939d033d9747aedc19bbe
ssj4dofuturo <felip3.fl@gmail.com>
2021-02-15 15:33:07 -0300

    Aula 77 Correcao

--- lista_tarefa/lib/main.dart
+++ lista_tarefa/lib/main.dart
@@ -144,6 +144,7 @@ class _HomeState extends State<Home> {
             ),
             duration: Duration(seconds: 2),
           );
+          Scaffold.of(context).removeCurrentSnackBar();    // ADICIONE ESTE COMANDO
           Scaffold.of(context).showSnackBar(snack);
         });
       },
