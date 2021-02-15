3e18405dbae3de45b609e0864b4a38ba6137b3f6
ssj4dofuturo <felip3.fl@gmail.com>
2021-02-15 14:28:50 -0300

    Aula 72

--- lista_tarefa/lib/main.dart
+++ lista_tarefa/lib/main.dart
@@ -15,7 +15,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = ["Daniel","Marcos","Felipe","João","Paulo"];
+
+  final _toDoController = TextEditingController();
+  List _toDoList = [];
+
+  void _addTodo(){
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +45,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                   child: TextField(
+                    controller: _toDoController,
                   decoration: InputDecoration(
                       labelText: "Nova Tarefa",
                       labelStyle: TextStyle(color: Colors.blueAccent)
@@ -42,7 +55,7 @@ class _HomeState extends State<Home> {
                   color: Colors.blueAccent,
                   child: Text("ADD"),
                   textColor: Colors.white,
-                  onPressed: (){},
+                  onPressed: _addTodo,
                 )
               ],
             ),
@@ -59,6 +72,11 @@ class _HomeState extends State<Home> {
                         child: Icon(_toDoList[index]["ok"] ?
                         Icons.check : Icons.error
                         ),),
+                      onChanged: (c){
+                        setState(() {
+                          _toDoList[index]["ok"] = c;
+                        });
+                      },
                     );
                   }),
           )
