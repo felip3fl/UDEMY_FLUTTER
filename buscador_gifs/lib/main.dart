aad049f3adbc860371b5c7475be450eefc59e947
ssj4dofuturo <felip3.fl@gmail.com>
2021-02-18 20:49:13 -0300

    Aula 83

--- buscador_gifs/lib/main.dart
+++ buscador_gifs/lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage()
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
   ));
 }
 
