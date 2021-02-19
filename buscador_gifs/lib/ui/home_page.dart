8732841ece4ec11c4c11e2b4d3a6f6200e75cf8d
ssj4dofuturo <felip3.fl@gmail.com>
2021-02-19 16:07:23 -0300

    Aula 84

--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -10,27 +10,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
   int _offset = 0;
-  Future<Map> _getGifs() async{
 
+  Future<Map> _getGifs() async {
     http.Response response;
 
-    if(_search == null)
-      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=z5IoGted3z1ylvM38unbMmerWVGQGs7R&limit=20&rating=g');
+    if (_search == null)
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/trending?api_key=z5IoGted3z1ylvM38unbMmerWVGQGs7R&limit=20&rating=g');
     else
-      response = await http.get('https://api.giphy.com/v1/gifs/search?api_key=z5IoGted3z1ylvM38unbMmerWVGQGs7R&q=$_search&limit=20&offset=$_offset&rating=g&lang=en');
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/search?api_key=z5IoGted3z1ylvM38unbMmerWVGQGs7R&q=$_search&limit=20&offset=$_offset&rating=g&lang=en');
 
     return json.decode(response.body);
   }
-
 
   @override
   void initState() {
     super.initState();
 
-    _getGifs().then((map){
+    _getGifs().then((map) {
       print(map);
     });
   }
@@ -40,24 +40,55 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
         centerTitle: true,
       ),
       backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
-            Padding(padding: EdgeInsets.all(10.0),
-            child:           TextField(
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
               decoration: InputDecoration(
                   labelText: "Pesquise Aqui!",
                   labelStyle: TextStyle(color: Colors.white),
-                  border: OutlineInputBorder()
-              ),
+                  border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
-            ),)
+            ),
+          ),
+          Expanded(
+              child: FutureBuilder(
+                future: _getGifs(),
+            builder: (context, snapshot){
+                  switch(snapshot.connectionState){
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      );
+                    default:
+                      if(snapshot.hasError)
+                        return Container();
+                      else
+                        _createGifTable(context, snapshot);
+                  }
+            },
+          )),
         ],
       ),
     );
   }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+
+  }
+
 }
