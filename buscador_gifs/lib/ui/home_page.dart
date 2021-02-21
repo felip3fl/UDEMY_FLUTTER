054751bae3d7af81cf6faea2d36507e138a2128e
ssj4dofuturo <felip3.fl@gmail.com>
2021-02-21 14:08:25 -0300

    Aula 86

--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -60,26 +60,26 @@ class _HomePageState extends State<HomePage> {
           ),
           Expanded(
               child: FutureBuilder(
-                future: _getGifs(),
-            builder: (context, snapshot){
-                  switch(snapshot.connectionState){
-                    case ConnectionState.waiting:
-                    case ConnectionState.none:
-                      return Container(
-                        width: 200.0,
-                        height: 200.0,
-                        alignment: Alignment.center,
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                          strokeWidth: 5.0,
-                        ),
-                      );
-                    default:
-                      if(snapshot.hasError)
-                        return Container();
-                      else
-                        _createGifTable(context, snapshot);
-                  }
+            future: _getGifs(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                case ConnectionState.none:
+                  return Container(
+                    width: 200.0,
+                    height: 200.0,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      strokeWidth: 5.0,
+                    ),
+                  );
+                default:
+                  if (snapshot.hasError)
+                    return Container();
+                  else
+                    return _createGifTable(context, snapshot);
+              }
             },
           )),
         ],
@@ -87,8 +87,23 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
-
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+      ),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, index){
+          return GestureDetector(
+            child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+              fit: BoxFit.cover
+            ),
+          );
+      },
+    );
   }
-
 }
