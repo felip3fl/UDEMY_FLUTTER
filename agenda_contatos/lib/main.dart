eea2de3fc33f7c86205a5452276fd0d02fe9168b
ssj4dofuturo <felip3.fl@gmail.com>
2021-03-06 11:31:22 -0300

    Aula 102

--- agenda_contatos/lib/main.dart
+++ agenda_contatos/lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:agenda_contatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 
 import 'ui/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ContactPage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
